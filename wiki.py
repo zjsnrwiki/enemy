0883fd2b49054eb45b7db88364b42736da973852
liuzhe <cruise.pas@gmail.com>
2016-10-10 19:13:08 +0800

    wiki update and new 1-3 data

--- wiki.py
+++ wiki.py
@@ -1,6 +1,8 @@
 from collections import OrderedDict
 import json
 
+formationName = [ '', '单纵', '复纵', '轮形', '梯形', '单横' ]
+
 rangeName = [ '无', '短', '中', '长', '超长' ]
 
 simpleTypes = [ '', '航空母舰', '轻型航空母舰', '装甲航母', '战列舰', '航空战列舰', '战列巡洋舰', '重巡洋舰', \
@@ -13,10 +15,26 @@ def typeName(t):
         return '航空战列舰'
 
 def run():
-    ret = 'ships = { }\n\n'
-    #fleets = json.load(open('fleets.json'), object_pairs_hook = OrderedDict)
+    ret = 'fleets = { }\nships = { }\n\n'
+    fleets = json.load(open('fleets.json'), object_pairs_hook = OrderedDict)
     ships = json.load(open('ships.json'), object_pairs_hook = OrderedDict)
 
+    ret += '-' * 100 + '\n\n'
+    for node, data in fleets.items():
+        if node[0] == 'C': continue  # TODO
+        ret += "fleets['%s'] = { }\n" % node
+        i = 0
+        for f in data.values():
+            i += 1
+            ret += "fleets['%s'][%d] = { " % (node, i)
+            ret += "title='%s', " % f['title']
+            ret += "formation='%s', " % formationName[f['formation']]
+            ret += "ships={ '"
+            ret += "', '".join(str(s) for s in f['ships'])
+            ret += "' } }\n"
+        ret += '\n'
+
+    ret += '-' * 100 + '\n\n'
     for id_, ship in ships.items():
         ret += "ships['%s'] = {\n    " % id_
         ret += "title='%s', " % ship['title']
@@ -43,7 +61,7 @@ def run():
         ret += "cap3=%d, " % ship['cap'][2]
         ret += "cap4=%d\n}\n\n" % ship['cap'][3]
 
-    ret += 'return ships'
+    ret += '-' * 100 + '\n\nreturn { fleets=fleets, ships=ships }'
     return ret
 
 if __name__ == '__main__':
