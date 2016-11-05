ab01bf39ebce75e6c675debb56521231e04241f0
liuzhe <cruise.pas@gmail.com>
2016-11-06 04:33:04 +0800

    3-4

--- run.sh
+++ run.sh
@@ -2,5 +2,6 @@ adb pull /sdcard/download/$1 .
 python decode.py $1 $1_decode
 python load.py $1_decode
 python wiki.py
-mkdir -p data
-mv $1 $1_decode data
+mkdir -p data/orig
+mv $1 data/orig
+mv $1_decode data
